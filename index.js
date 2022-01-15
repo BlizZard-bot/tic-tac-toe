ed72c61cd6db0fd64c22f11f4fc4ddb11c63d4ca
Muhammad Raza Khan <muhammadraza9763@gmail.com>
2022-01-15 14:14:41 +0530

    Add functionality for getting player data and displaying markers

--- index.js
+++ index.js
@@ -12,36 +12,139 @@
   });
 })();
 
-function returnToHomePage() {
-  const returnBtn = document.querySelector(".return-btn");
-  let destination = "home";
-  returnBtn.addEventListener("click", changeScreen.bind(null, destination));
-}
+// Main functionality
 
-(function moveToGame() {
-  const startGameBtn = document.querySelector(".start-game-btn");
-  let destination = "game";
-  startGameBtn.addEventListener("click", changeScreen.bind(null, destination));
+// GameBoard
+const gameBoard = (() => {
+  const gameBoardArr = [];
+  return { gameBoardArr };
 })();
 
-function changeScreen(destination) {
-  const helpPrompt = document.querySelector(".help-prompt");
-  const header = document.querySelector(".header");
-  const startingPage = document.querySelector(".starting-page");
-  const startGameBtn = document.querySelector(".start-game-btn");
-  const gameSection = document.querySelector(".game-section");
-  if (destination === "home") {
-    helpPrompt.classList.remove("hidden");
-    header.classList.remove("hidden");
-    startingPage.classList.remove("hidden");
-    startGameBtn.classList.remove("hidden");
-    gameSection.classList.add("hidden");
-  } else if (destination === "game") {
-    helpPrompt.classList.add("hidden");
-    header.classList.add("hidden");
-    startingPage.classList.add("hidden");
-    startGameBtn.classList.add("hidden");
-    gameSection.classList.remove("hidden");
-    returnToHomePage();
+// Player factory
+const Player = (type, marker) => {
+  return { type, marker };
+};
+
+// Display Control
+
+const displayController = (() => {
+  const displayMarkerOnClick = () => {
+    const gameBoardArr = gameBoard.gameBoardArr;
+    const mainGrid = document.querySelector(".main-grid");
+    mainGrid.addEventListener("click", (e) => {
+      if (e.target.classList.contains("grid-cell")) {
+        displayMarker(e.target, gameBoardArr);
+      }
+    });
+  };
+
+  const displayMarker = (cell, gameBoardArr) => {
+    for (let i in gameBoardArr) {
+      if (+cell.getAttribute("data-number") - 1 === +i) {
+        cell.textContent = gameBoardArr[i];
+        showMarkerColor(cell);
+      }
+    }
+  };
+
+  const showMarkerColor = (cell) => {
+    if (cell.textContent === "X") {
+      cell.style.color = "var(--bg-btn)";
+    } else {
+      cell.style.color = "var(--marker-color)";
+    }
+  };
+  return { displayMarkerOnClick };
+})();
+
+displayController.displayMarkerOnClick();
+
+const gameControl = (() => {
+  const players = {};
+  function returnToHomePage() {
+    const returnBtn = document.querySelector(".return-btn");
+    let destination = "home";
+    returnBtn.addEventListener("click", changeScreen.bind(null, destination));
   }
-}
+
+  function moveToGame() {
+    const startGameBtn = document.querySelector(".start-game-btn");
+    let destination = "game";
+    startGameBtn.addEventListener("click", () => {
+      changeScreen(destination);
+    });
+  }
+
+  function changeScreen(destination) {
+    const helpPrompt = document.querySelector(".help-prompt");
+    const header = document.querySelector(".header");
+    const startingPage = document.querySelector(".starting-page");
+    const startGameBtn = document.querySelector(".start-game-btn");
+    const gameSection = document.querySelector(".game-section");
+    if (destination === "home") {
+      helpPrompt.classList.remove("hidden");
+      header.classList.remove("hidden");
+      startingPage.classList.remove("hidden");
+      startGameBtn.classList.remove("hidden");
+      gameSection.classList.add("hidden");
+    } else if (
+      destination === "game" &&
+      players.playerOne &&
+      players.playerTwo
+    ) {
+      helpPrompt.classList.add("hidden");
+      header.classList.add("hidden");
+      startingPage.classList.add("hidden");
+      startGameBtn.classList.add("hidden");
+      gameSection.classList.remove("hidden");
+      returnToHomePage();
+    } else {
+      alert("Select your players first");
+    }
+  }
+
+  function setPlayerData() {
+    const startingPage = document.querySelector(".starting-page");
+    startingPage.addEventListener("click", (e) => {
+      getPlayerData(e.target, players);
+    });
+  }
+
+  function getPlayerData(item, players) {
+    if (item.textContent === "Player" || item.textContent === "Bot") {
+      const marker =
+        item.parentElement.parentElement.firstElementChild.textContent;
+      const type = item.textContent;
+      styleSelectedButton(item);
+      if (marker === "X") {
+        players.playerOne = Player(type, marker);
+      } else {
+        players.playerTwo = Player(type, marker);
+      }
+    }
+    console.log(players);
+  }
+
+  function styleSelectedButton(button) {
+    if (
+      button.textContent === "Player" &&
+      !button.classList.contains("selected")
+    ) {
+      button.nextElementSibling.classList.remove("selected");
+      button.classList.add("selected");
+    } else if (
+      button.textContent === "Bot" &&
+      !button.classList.contains("selected")
+    ) {
+      button.previousElementSibling.classList.remove("selected");
+      button.classList.add("selected");
+    }
+  }
+
+  const getPlayers = () => players;
+
+  return { setPlayerData, moveToGame, getPlayers };
+})();
+
+gameControl.setPlayerData();
+gameControl.moveToGame();
